3d991ed7d828b86ccae2909ab4720d73c2a1ace7
Satyajit Barik <117843736+satyajit7679@users.noreply.github.com>
2026-02-09 07:29:37 +0530

    Time: 365 ms (94.76%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-SELECT Views.author_id AS id FROM Views WHERE author_id = viewer_id GROUP BY author_id ORDER BY id;
+SELECT Views.author_id 
+AS id FROM Views 
+WHERE author_id = viewer_id 
+GROUP BY author_id ORDER BY id;
